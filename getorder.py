49389de60a0fe0f2a5b257005db2a29533430cc3
Omayeli Arenyeka <aomayeli@gmail.com>
2017-11-18 16:24:01 -0800

    Adding order data to data object

--- getorder.py
+++ getorder.py
@@ -1,11 +1,3 @@
-# women
-# ['mother', 'mom', 'mum', 'parent']
-# ['partner', 'wife', 'spouse', 'married']
-# <--------->
-# men
-# # ['father', 'dad', 'parent']
-# ['husband', 'partner', 'spouse', 'married']
-
 womanWithSpouse = 'partner|wife|spouse|married'
 womanParent = 'mother|mom|mum|parent'
 manWithSpouse = 'husband|partner|spouse|married'
@@ -15,18 +7,55 @@ data = {
 	"total": 0,
 	"women": {
 		"total": 0,
-		"spouse": 0,
-		"parent": 0,
-		"work": 0
+		"spouseAndParent": {
+			"total": 0
+		},
+		"spouse": {
+			"total": 0,
+			"only": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		},
+		"parent": {
+			"total": 0,
+			"only": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		},
+		"other": {
+			"total": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		}
 	},
 	"men": {
 		"total": 0,
-		"spouse": 0,
-		"parent": 0,
-		"work": 0
+		"spouseAndParent": 0,
+		"spouse": {
+			"total": 0,
+			"only": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		},
+		"parent": {
+			"total": 0,
+			"only": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		},
+		"other": {
+			"total": 0,
+			"first": 0,
+			"second": 0,
+			"third": 0
+		}
 	}
 }
-
 
 import pandas as pd
 pd.set_option("display.width", 120)
@@ -42,11 +71,23 @@ def addCountsToData():
 
 	# woman as spouse
 	df_womanWithSpouse = df[women & (df['bio'].str.contains(womanWithSpouse, case=False))]
-	data["women"]["spouse"] = len(df_womanWithSpouse)
+	data["women"]["spouse"]["total"] = len(df_womanWithSpouse)
 
 	# woman as parent
 	df_womanAsParent = df[women & (df['bio'].str.contains(womanParent, case=False))]
-	data["women"]["parent"] = len(df_womanAsParent)
+	data["women"]["parent"]["total"] = len(df_womanAsParent)
+	
+	# both
+	df_womenParentAndSpouse = df[women & ((df['bio'].str.contains(womanParent, case=False)) & (df['bio'].str.contains(womanWithSpouse, case=False)))]
+	data["women"]["spouseAndParent"] = len(df_womenParentAndSpouse)
+	
+	# either parent or spouse
+	data["women"]["spouse"]["only"] = len(df_womanWithSpouse) - len(df_womenParentAndSpouse)
+	data["women"]["parent"]["only"] = len(df_womanAsParent) - len(df_womenParentAndSpouse)
+
+	# only other
+	df_womenParentOrSpouse = df[women & ((df['bio'].str.contains(womanParent, case=False)) | (df['bio'].str.contains(womanWithSpouse, case=False)))]
+	data["women"]["other"]["total"] = len(df_women) - len(df_womenParentOrSpouse)
 
 	#  ~ Men ~
 	men = df['gender'] == "male"
@@ -55,43 +96,95 @@ def addCountsToData():
 
 	# man as spouse
 	df_manWithSpouse = df[men & (df['bio'].str.contains(manWithSpouse, case=False))]
-	data["men"]["spouse"] = len(df_manWithSpouse)
+	data["men"]["spouse"]["total"] = len(df_manWithSpouse)
 
 	# man as parent
 	df_manAsParent = df[men & (df['bio'].str.contains(manParent, case=False))]
-	data["men"]["parent"] = len(df_manAsParent)
+	data["men"]["parent"]["total"] = len(df_manAsParent)
+	
+	# both
+	df_menParentAndSpouse = df[men & ((df['bio'].str.contains(manParent, case=False)) & (df['bio'].str.contains(manWithSpouse, case=False)))]
+	data["men"]["spouseAndParent"] = len(df_menParentAndSpouse)
+	
+	# either parent or spouse
+	data["men"]["spouse"]["only"] = len(df_manWithSpouse) - len(df_menParentAndSpouse)
+	data["men"]["parent"]["only"] = len(df_manAsParent) - len(df_menParentAndSpouse)
 
-	# print data
+
+	# only other
+	df_menParentOrSpouse = df[men & ((df['bio'].str.contains(manParent, case=False)) | (df['bio'].str.contains(manWithSpouse, case=False)))]
+	data["men"]["other"]["total"] = len(df_men) - len(df_menParentOrSpouse)
+
+
+def getIndexOfNames(bio, names):
+	arrayOfNames = names.split('|')
+	wordFoundAt = -1
+	for word in arrayOfNames:
+		if bio.find(word) != 1:
+			wordFoundAt = bio.find(word)
+			break
+	return wordFoundAt
+
+def compareIndexes(spouseIndex, parentIndex, key):
+	# if bio has references to both being a spouse and parent
+	if (spouseIndex > -1 & parentIndex > -1):
+		# both
+		# spouse comes before
+		if (spouseIndex < parentIndex):
+			if spouseIndex == 0:
+				# first of all
+				data[key]["spouse"]["first"]+=1
+				data[key]["parent"]["second"]+=1
+				data[key]["other"]["third"]+=1
+
+			else:
+				data[key]["other"]["first"]+=1
+				data[key]["spouse"]["second"]+=1
+				data[key]["parent"]["third"]+=1
+		else:
+			if parentIndex == 0:
+				data[key]["parent"]["first"]+=1
+				data[key]["spouse"]["second"]+=1
+				data[key]["other"]["third"]+=1
+
+			else:
+				data[key]["other"]["first"]+=1
+				data[key]["parent"]["second"]+=1
+				data[key]["spouse"]["third"]+=1
+	elif (spouseIndex > -1): 
+		# just spouse reference
+		if spouseIndex == 0:
+			data[key]["spouse"]["first"]+=1
+			data[key]["other"]["second"]+=1
+		else:
+			data[key]["other"]["first"]+=1
+			data[key]["spouse"]["second"]+=1
+	elif (parentIndex > -1): 
+		# just parent reference
+		if parentIndex == 0:
+			data[key]["parent"]["first"]+=1
+			data[key]["other"]["second"]+=1
+		else:
+			data[key]["other"]["first"]+=1
+			data[key]["parent"]["second"]+=1
 
 def getOrderOfBio():
 	for row in df.iterrows():
 		data = row[1]
 		bio = data['bio']
 		gender = data['gender']
-
-
-# def bioContainsSpouse(bio, spouseArray):
-# 	print bio
-# 	spouseWordFound = -1
-# 	# for word in spouseArray:
-# 	# 	if bio.find(word) != 1:
-# 	# 		spouseWordFound = bio.find(word)
-# 	# 		break
-# 	if spouseWordFound > -1:
-# 		return True
-# 	else:
-# 		return False
-
-# def bioContainsParent(bio, parentArray):
-# 	parentWordFound = -1
-# 	for word in parentArray:
-# 		if bio.find(word) != 1:
-# 			parentWordFound = bio.find(word)
-# 			break
-# 	if parentWordFound > -1:
-# 		return True
-# 	else:
-# 		return False
+		if gender == 'female':
+			# get index for spouse
+			spouseIndex = getIndexOfNames(bio, womanWithSpouse)
+			# get index for parent
+			parentIndex = getIndexOfNames(bio, womanParent)
+			compareIndexes(spouseIndex, parentIndex, 'women')
+		else:
+			spouseIndex = getIndexOfNames(bio, manWithSpouse)
+			parentIndex = getIndexOfNames(bio, manParent)
+			compareIndexes(spouseIndex, parentIndex, 'men')
 
 addCountsToData();
 getOrderOfBio();
+print data
+
